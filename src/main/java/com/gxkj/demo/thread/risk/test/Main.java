b1c523b3d894751a947836d88dbdacd913a5a08e
01jiangwei01 <01jiangwei01@163.com>
2015-03-03 19:41:07 +0800

    主程序等待线程完成工作后再执行

--- src/main/java/com/gxkj/demo/thread/risk/test/Main.java
+++ src/main/java/com/gxkj/demo/thread/risk/test/Main.java
@@ -1,17 +1,26 @@
 package com.gxkj.demo.thread.risk.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
 
 
 public class Main {
 	 public static Vector<Integer> dbDataV = new Vector<Integer>();
-	 
-	public static void main(String[] args) {
-		int l = 20;
-		for(int i=0;i<l;i++){
-			RiskRouteService s = new RiskRouteService(i+1);
+	 final static SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
+	public static void main(String[] args) throws InterruptedException {
+		int threadNum = 20;
+		
+		CountDownLatch latch = new CountDownLatch(threadNum);//初始化countDown  
+		long begintime = System.currentTimeMillis();
+		for(int i=0;i<threadNum;i++){
+			RiskRouteService s = new RiskRouteService(latch,i+1);
 			Thread thread = new Thread(s);
 			thread.start();
 		}
+		long endintime = System.currentTimeMillis();
+		 latch.await();//等待所有工人完成工作 
+		 System.out.println("所有工作结束："+sdf.format(new Date())+";耗时："+(endintime-begintime));;  
 	}
 }
--- src/main/java/com/gxkj/demo/thread/risk/test/RiskRouteService.java
+++ src/main/java/com/gxkj/demo/thread/risk/test/RiskRouteService.java
@@ -1,19 +1,27 @@
 package com.gxkj.demo.thread.risk.test;
 
 import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
 
 import com.gxkj.demo.thread.risk.Task;
 
 public class RiskRouteService extends Task {
 	private int id = 0;
+	/**
+	 * 多线程技术器
+	 */
+	CountDownLatch latch ; 
 	
-	public RiskRouteService(int id) {
+	public RiskRouteService(CountDownLatch latch,int id) {
 		super();
 		this.id = id;
+		this.latch = latch;
 	}
 
 	public void run() {
 		System.out.println(String.format("RiskRouteService——%d执行", id));
 		Main.dbDataV.add(this.id);
+		
+		 latch.countDown();//工人完成工作，计数器减一  
 	}
 }
